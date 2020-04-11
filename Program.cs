4e4174cec154851ec5537478f67b8831dbec73ad
bagas <bagas.nugroho@students.amikom.ac.id>
2020-04-11 11:09:42 +0700

    Inheritance

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace baru.LatihanInheritance
+{
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Person person = new Person("Supri", 19);
+            person.GetNameAndAge();
+
+            Teacher teacher = new Teacher("Huda", 30, "2122", "Math");
+            teacher.GetNameAndAge();
+
+            Student student = new Student("Dias", 20, "2314", "Diaskuncoro@gmail.com");
+            student.GetNameAndAge();
+        }
+    }
+}
